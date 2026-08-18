a1a22bd65e8e6c051852b9d710dea6b259db80ce
Alan Ibarra <alanjibarradev@gmail.com>
2026-08-18 14:24:42 -0700

    feat: init next.js turborepo and tailwind

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -20,7 +20,7 @@ const ThemeImage = (props: Props) => {
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-red-500">
       <main className={styles.main}>
         <ThemeImage
           className={styles.logo}
